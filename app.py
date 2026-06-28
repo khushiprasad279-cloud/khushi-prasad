bf2b62b11e782210ba9653c094a681cdde0b6722
khushiprasad279-cloud <khushiprasad279@gmail.com>
2026-06-28 14:17:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+# Secure File Encryption Tool
+
+## Features
+- AES-based secure encryption (via Fernet)
+- File encryption and decryption
+- Python implementation
+- Easy to use
+
+## Install
+
+pip install -r requirements.txt
+
+## Run
+
+python app.py
